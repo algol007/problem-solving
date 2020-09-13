0c107d23751e72eab4728a66f9f89ff35fb6b0e9
algol007 <adhyformerz@gmail.com>
2020-09-13 22:37:50 +0700

    a very big sum

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-const a = [17, 28, 30]
-const b = [99, 16, 8]
-
-const compare = (a, b) => {
-  let data = [0, 0]
-  for(let i=0; i<a.length; i++) {
-      if(a[i] > b[i]) {
-        data[0]++
-      }
-      else if (a[i] < b[i]) {
-        data[1]++
-      }
-    }
-  return data
+const bigSumArray = (arr) => {
+  return ar.reduce((a, b) => a + b, 0)
 }
-
-console.log(compare(a,b))
-
