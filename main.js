a0fbc9ccf84e123d239bd4cec734e89663340e79
algol007 <adhyformerz@gmail.com>
2020-09-13 22:05:04 +0700

    compare the triplets

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-function simpleArraySum(ar) {
-  return ar.reduce((a, b) => a + b)
+const a = [17, 28, 30]
+const b = [99, 16, 8]
+
+const compare = (a, b) => {
+  let data = [0, 0]
+  for(let i=0; i<a.length; i++) {
+      if(a[i] > b[i]) {
+        data[0]++
+      }
+      else if (a[i] < b[i]) {
+        data[1]++
+      }
+    }
+  return data
 }
 
-console.log(simpleArraySum([1,2,3]))
+console.log(compare(a,b))
+
